4f5c588a7b1e562041153f5ac67783ff841d8d0c
Chris Storz <83615348+chrisjws-harness@users.noreply.github.com>
2023-12-07 07:09:38 -0800

    Update app.py

--- app.py
+++ app.py
@@ -23,9 +23,9 @@ def get_temperature():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-def fetch_temperature(city, units):
+def fetch_temperature(city_name, units="imperial"):
     base_url = 'http://api.openweathermap.org/data/2.5/weather'
-    params = {'q': city, 'units': units, 'appid': OPENWEATHER_API_KEY}
+    params = {'q': city_name, 'units': units, 'appid': OPENWEATHER_API_KEY}
 
     response = requests.get(base_url, params=params)
     data = response.json()
@@ -36,6 +36,20 @@ def fetch_temperature(city, units):
     else:
         error_message = data.get('message', 'Unknown error')
         raise Exception(f'Failed to fetch temperature: {error_message}')
+
+def get_temperature(city_name, units="imperial"):
+    # Load city data from the JSON file
+    with open("cities_data.json", "r") as file:
+        cities_data = json.load(file)
+
+    # Capitalize the first letter of each word in the city name
+    formatted_city = ' '.join(word.capitalize() for word in city_name.split())
+    
+    for city in cities_data["cities"]:
+        if city["name"] == formatted_city:
+            return city["temperature"][units]
+
+    raise ValueError(f"City '{formatted_city}' not found in the list")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
